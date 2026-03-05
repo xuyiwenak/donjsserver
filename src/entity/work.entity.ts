2a52d94faf06bebb74b3fbafe042d25fe5d90701
ivan <Ivan@dinoverse.games>
2026-03-05 17:36:17 +0800

    登录

---
+++ src/entity/work.entity.ts
@@ -0,0 +1,38 @@
+import { Schema } from "mongoose";
+
+export interface IWork {
+  workId: string;
+  authorId?: string | null;
+  desc: string;
+  images: { url: string; name: string; type: string }[];
+  tags: string[];
+  location?: string;
+  status: "draft" | "published";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const WorkSchema = new Schema<IWork>(
+  {
+    workId: { type: String, required: true, unique: true },
+    authorId: { type: String },
+    desc: { type: String, required: true },
+    images: [
+      {
+        url: { type: String, required: true },
+        name: { type: String, required: true },
+        type: { type: String, required: true },
+      },
+    ],
+    tags: [{ type: String }],
+    location: { type: String },
+    status: {
+      type: String,
+      enum: ["draft", "published"],
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true },
+);
+
